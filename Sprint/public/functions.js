13ba02e28e4267ff2be5783607a3f87af55a9a05
jeries <geriesmazzawi@gmail.com>
2020-08-12 19:12:44 +0300

    Firstt step

---
+++ Sprint/public/functions.js
@@ -0,0 +1,30 @@
+let table=document.getElementById('selectedData')
+
+const getTasks= new Promise((resolve, reject) => {
+    fetch('/Tickets')
+        .then(res => res.json())
+        .then(task => resolve(task))
+  });
+
+(async()=>{
+    const tasks= await getTasks;
+    /*let row=table.insertRow();*/
+   /* let cell=row.insertCell();
+    let cell1=row.insertCell();
+    let cell2=row.insertCell();*/
+    let taskList = "";
+    tasks.forEach(item => {
+        let row=table.insertRow();
+        let cell=row.insertCell();
+        let cell1=row.insertCell();
+        let cell2=row.insertCell();
+       cell.innerHTML=`${item.id}`
+       cell1.innerHTML=`${item.name}`
+       cell2.innerHTML=`${item.oldvalue}`
+       
+    
+});
+   
+/*document.getElementById("selectedData").innerHTML = namesList;*/
+    
+})();
---
+++ Sprint/server.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+const url = "mongodb+srv://jeries:g1g2g3g4g5@cluster0.sb6dm.mongodb.net/test";
+
+
+const mongoose = require('mongoose');
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+
+
+app.use(express.static('public'));
+
+const Task = mongoose.model('Task', {
+    jiraItem: {
+      jiraID: String,
+      jiraName: String,
+      jiraType: String,
+      priority: String,
+      status: String,
+      specialFields: {
+        jiraParentId: String,
+        functionalTest: Boolean,
+        qaRepresentative: String,
+        fixVersion: String
+      }
+    },
+    qcItem: {
+      requirmentId: String,
+      creationTime: {
+        type: Date
+      },
+      requirmentType: String,
+      status: String
+    },
+    diffItem: {
+      type: { type: String },
+      updatedTime: Date,
+      updatedFields: String,
+      fieldName:String,
+      oldVal: String,
+      newVal: String
+    },
+   /* taskItem: {
+      user: User,
+      isDone: Boolean,
+      updatedTime: Date
+    }*/
+  });
+
+
+
+  app.get('/Tickets', (req, res) => {
+    res.send(Task)
+})
+
+  app.listen(3000 , ()=> {
+    console.log("Port is running in 3000")
+})
