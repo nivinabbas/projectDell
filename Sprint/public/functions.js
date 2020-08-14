7db784a3a4d6be39a069edd8e2da570513b4a61b
Nivin <nivin.abbas1@gmail.com>
2020-08-14 03:12:55 +0300

    design updated

--- Sprint/public/functions.js
+++ Sprint/public/functions.js
@@ -1,6 +1,4 @@
-const elmtTable = document.getElementById('selectedData');
-
-
+const elmtTable = document.getElementById("selectedData");
 
 /*const getTasks= new Promise((resolve, reject) => {
     fetch('/Tickets')
@@ -8,36 +6,31 @@ const elmtTable = document.getElementById('selectedData');
         .then(task => resolve(task))
   });*/
 
-
-
 function getData(event) {
-    event.preventDefault();
-   elmtTable.innerHTML=`<tr>
+  event.preventDefault();
+  elmtTable.innerHTML = `<tr>
    <td class="title">TaskId</td>
    <td class="title">Date&time</td>
    <td class="title">New value</td>
   
 </tr>`;
-    fetch('/getUpdatedByStatus')
-        .then(res => res.json())
-        .then(data => {
-            
-            data.forEach(element => {
-              console.log(element)
-              console.log(element.tasks)
-              element.tasks.forEach(task =>{
-                console.log(task.diffItem.updatedFields)
-                  console.log(task.diffItem.updatedFields[0])
-                  let row=elmtTable.insertRow();
-                  let cell=row.insertCell();
-                  let cell1=row.insertCell();
-                  let cell2=row.insertCell();
-                 cell.innerHTML=`${task.jiraItem.jiraId}`
-                 cell1.innerHTML=`${task.diffItem.updateTime}`
-                 cell2.innerHTML=`${task.diffItem.updatedFields[0].newValue}`
-              })
-                
-            });
-           
-        })
-  }
+  fetch("/getUpdatedByStatus")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((element) => {
+        console.log(element);
+        console.log(element.tasks);
+        element.tasks.forEach((task) => {
+          console.log(task.diffItem.updatedFields);
+          console.log(task.diffItem.updatedFields[0]);
+          let row = elmtTable.insertRow();
+          let cell = row.insertCell();
+          let cell1 = row.insertCell();
+          let cell2 = row.insertCell();
+          cell.innerHTML = `${task.jiraItem.jiraId}`;
+          cell1.innerHTML = `${task.diffItem.updateTime}`;
+          cell2.innerHTML = `${task.diffItem.updatedFields[0].newValue}`;
+        });
+      });
+    });
+}
