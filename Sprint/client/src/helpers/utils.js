37e9ab40eb4b7e98f257ab579f3ec149cfa5ffef
Nivin <nivin.abbas1@gmail.com>
2020-08-23 05:28:48 +0300

    update UI obj

--- Sprint/client/src/helpers/utils.js
+++ Sprint/client/src/helpers/utils.js
@@ -13,3 +13,20 @@ export const formatDate = (date) => {
   ] = dateTimeFormat.formatToParts(date);
   return `${day}-${month}-${year}`;
 };
+
+export const prepareDateForRequest = (date) => {
+  const dateTimeFormat = new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "numeric",
+    day: "2-digit",
+  });
+  const [
+    { value: m },
+    ,
+    { value: d },
+    ,
+    { value: y },
+  ] = dateTimeFormat.formatToParts(date);
+  console.log([y, m, d]);
+  return [y, m - 1, d]; //Return as an array with y,m,d sequence
+};
